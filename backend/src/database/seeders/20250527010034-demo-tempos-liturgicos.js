255b64d6f551b1777d4dc66eda122e998a6017d8
jessicarolyne <jessicarolyne@hotmail.com>
2025-05-26 22:09:14 -0300

    :sparkles: feat: Criação seeder Tempos liturgicos

---
+++ backend/src/database/seeders/20250527010034-demo-tempos-liturgicos.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('tempos_liturgicos', [{
+       nome: 'Tempo Comum',
+       cor: 'Verder',
+       descricao: null
+     }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+     await queryInterface.bulkDelete('tempos_liturgicos', null, {});
+  }
+};
