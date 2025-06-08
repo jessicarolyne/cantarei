585ccd3c09fc53ff177f406c7f98a665f66efdb0
jessicarolyne <jessicarolyne@hotmail.com>
2025-06-08 19:04:10 -0300

    :sparkles: feat: Banner da Home

---
+++ frontend/src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css';
+function Banner () {
+return (
+    <header className='banner'>
+        <img src="/images/Banner.png" alt="Banner principal da Home"/>
+    </header>
+)
+}
+
+export default Banner
